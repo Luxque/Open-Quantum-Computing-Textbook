08813f009510fbe5bdf3191d2f7beb67f3b330b1
Daniel Park <parkdaniel1002@gmail.com>
2025-12-12 19:12:58 -0600

    Add `open.ps1`

---
+++ script/open.ps1
@@ -0,0 +1,20 @@
+param (
+  [Parameter(Mandatory=$true, Position=0)]
+  [string]$Lang
+)
+
+$BookRoot = Join-Path -Path $PSScriptRoot -ChildPath "..\book" | Resolve-Path -Relative
+$BookPath = Join-Path -Path $BookRoot -ChildPath $Lang
+
+
+function Open-Book {
+  param (
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$Path
+  )
+
+  mdbook serve $Path --open --watcher=poll
+}
+
+
+Open-Book -Path $BookPath
